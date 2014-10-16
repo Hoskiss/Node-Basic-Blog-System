a04d250de6831fb4bdbd8f00b5b9e6c86f1762b2
Hoskiss <hoskiss@gmail.com>
2014-10-16 11:22:33 +0800

    init page

---
+++ routes/user.js
@@ -0,0 +1,65 @@
+/* GET users listing. */
+
+exports.list = function(req, res){
+    res.send("respond with a resource");
+};
+
+/* GET users request. */
+
+exports.register = function(req, res){
+    if (req.session.logined) {
+        res.redirect('/');
+        return;
+    }
+    res.render("users/register");
+};
+
+exports.signin = function(req, res){
+    if (req.session.logined) {
+        res.redirect('/');
+        return;
+    }
+    res.render("users/signin");
+};
+
+exports.signout = function(req, res){
+    req.session.logined = false;
+    res.redirect('/');
+    res.end();
+};
+
+exports.forget = function(req, res){
+    if (req.session.logined) {
+        res.redirect('/');
+        return;
+    }
+    res.render("users/forget");
+};
+
+exports.profile = function(req, res){
+    res.send("This is the profile page.");
+};
+
+exports.add_article = function(req, res){
+    res.send("This is the add_article page.");
+};
+
+exports.modify = function(req, res){
+    res.send("This is the modify page.");
+};
+
+exports.message = function(req, res){
+    res.send("This is the message page.");
+};
+
+exports.login = function(req, res){
+    if((!req.body.user) || (!req.body.passwd)) {
+        res.redirect('register');
+        return;
+    }
+
+    req.session.name = req.body.user;
+    req.session.passwd = req.body.passwd;
+    req.session.logined = true;
+    req.redirect('/');
+};
